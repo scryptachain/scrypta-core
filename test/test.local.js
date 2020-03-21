935132b68781046b2a07422fc3ce348768bd09af
turinglabsorg <turinglabs@criptext.com>
2020-03-21 10:02:31 +0100

    started testing

--- test/test.local.js
+++ test/test.local.js
@@ -1,6 +1,48 @@
 let ScryptaCore = require('../src/index.js')
-
 let scrypta = new ScryptaCore
+var assert = require('assert')
+const password = '123456'
+
+
+describe('Addresses', async function() {
+    it('Address should be length 34 bytes', async function(){
+        let address = await scrypta.createAddress(password, false)
+        assert.equal(34, address.pub.length);
+    })
+    it('PubKey should be length 66 bytes', async function(){
+        let address = await scrypta.createAddress(password, false)
+        assert.equal(66, address.key.length);
+    })
+    it('PrivKey should be length 52 bytes', async function(){
+        let address = await scrypta.createAddress(password, false)
+        assert.equal(52, address.prv.length);
+    })
+    it('Wallet store shuold be decryptable and address should be the same', async function(){
+        let address = await scrypta.createAddress(password, false)
+        let readed = await scrypta.readKey(password, address.walletstore)
+        assert.equal(readed.key, address.key);
+    })
+});
+
+describe('Idanodes', async function() {
+    it('Should GET first available IdaNode', function(){
+        return new Promise(async response => {
+            let getinfo = await scrypta.get('/wallet/getinfo')
+            if(getinfo.blocks !== undefined){
+                response(getinfo.blocks)
+            }
+        })
+    })
+
+    it('Should POST first available IdaNode', async function(){
+        let Bob = await scrypta.createAddress(password, false)
+        let Alice = await scrypta.createAddress(password, false)
+        let trustlink = await scrypta.post('/trustlink/init', { addresses: Bob.key + ',' + Alice.key, airdrop: false})
+        assert.equal(34, trustlink.data.address.length);
+    })
+});
+
+/*
 // SHOULD CREATE ADDRESS
 let password = '123456'
 scrypta.createAddress(password, true).then(async res => {
@@ -25,4 +67,4 @@ scrypta.createAddress(password, true).then(async res => {
     setInterval(function(){
         scrypta.broadcast(res.pub, password, 'message', 'Now are '+ new Date() +'!')
     },2500)
-})
+})*/
